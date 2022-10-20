7290953be79ed537da4ec901d74ed373349f1647
diego-cavenati <diegocavenati@gmail.com>
2022-10-20 13:59:38 +0200

    milestone 0 - add objects to array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,44 @@
-/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
-MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
-MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
-BONUS 1: Trasformare la stringa foto in una immagine effettiva
-BONUS 2: Organizzare i singoli membri in card/schede */
+/* 
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 
+- MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+- MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
+- MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+- BONUS 1: Trasformare la stringa foto in una immagine effettiva
+- BONUS 2: Organizzare i singoli membri in card/schede 
+*/
+
+// Creare l’array di oggetti con le informazioni fornite
+const staff = [
+    {
+    name : "Wayne Barnett",
+    role : "Founder & CEO",
+    image : "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+    name : "Angela",
+    role : "Chief Editor",
+    image : "angela-caroll-chief-editor.jpg",
+    },
+    {
+    name : "Walter gordon",
+    role : "Chief Editor",
+    image : "walter-gordon-office-manager.jpg",
+    },
+    {
+    name : "Angela Lopez",
+    role : "Social Media Manager",
+    image : "angela-lopez-social-media-manager.jpg",
+    },
+    {
+    name : "Scott Estrada",
+    role : "Developer",
+    image : "scott-estrada-developer.jpg",
+    },
+    {
+    name : "Barbara Ramos",
+    role : "Graphic designer",
+    image : "barbara-ramos-graphic-designer.jpg",
+    }
+]
